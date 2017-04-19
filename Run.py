64537d4b2b98cb06506bc2e3ede975b96b8d84cf
Hero <www@eyenobig.com>
2017-04-19 19:22:02 +0800

    ok ok

--- Run.py
+++ Run.py
@@ -1,57 +1,76 @@
 import os
-
+import configparser
+import time, sys
+import win32com.client, win32api, win32con
+from key import VK_CODE
+from threading import Thread
+from danmu import DanMuClient
 #  生成 配置文件 如果有就退出
 
 settings = []
+test = ''
+shell = win32com.client.Dispatch("WScript.Shell")
+
+# 选择游戏
+GAME = os.listdir('game')[0]
+
+def pp(msg):
+    print(msg.encode(sys.stdin.encoding, 'ignore').decode(sys.stdin.encoding))
+def startemulator():
+    if os.path.splitext(os.listdir('game')[0])[1] != ".sgm"or".ini":
+        os.system('"%s\game\%s"' % (os.getcwd(), GAME))
+def press(*args):
+    '''
+    press, release
+    eg press('x', 'y', 'z')
+    '''
+    for i in args:
+        win32api.keybd_event(VK_CODE[i], 0, 0, 0)
+        if QUICK_PRESS == False:
+            time.sleep(0.2)
+        if QUICK_PRESS == True:
+            time.sleep(.01)
+        win32api.keybd_event(VK_CODE[i],0 ,win32con.KEYEVENTF_KEYUP ,0)
 
 while True:
     if os.path.isfile("settings.txt"):
         print("过段时间")
-        # config = configparser.ConfigParser()
-        # config.read("settings.txt")
-        # HOST = config.get('Settings', 'HOST')
-        # PORT = config.getint('Settings', 'PORT')
-        # AUTH = config.get('Settings', 'AUTH')
-        # NICK = config.get('Settings', 'USERNAME').lower()
-        # APP = config.get('Settings', 'APP')
-        # CHAT_CHANNEL = config.get('Settings', 'CHAT_CHANNEL').lower()	
-        # command_length = config.getint('Settings', 'LENGTH')
-        # QUICK_PRESS = config.getboolean('Settings', 'QUICK_PRESS')
+        config = configparser.ConfigParser()
+        config.read("settings.txt")
+
+        URL = config.get('Settings', 'LIVEROOM').lower()
+        APP = config.get('Settings', 'APP')
+        QUICK_PRESS = config.getboolean('Settings', 'QUICK_PRESS')
+        # CHAT_CHANNEL = config.get('Settings', 'CHAT_CHANNEL').lower()
+        command_length = config.getint('Settings', 'LENGTH')
+
         break
     else:
         print("Let's make you a config file")
-        settings.append("; Settings for Twitch Plays Pokemon bot")        
+        settings.append("; Settings for Twitch Plays Pokemon bot")
         settings.append("[Settings]\n")
-        
-        
-        settings.append("; Your Twitch Bot's Username")
-        print("Your Twitch Bot's Username")
-        settings_bot = input("Bot's Username: ")
-        settings.append("USERNAME = " + settings_bot + "\n")
-        
+
+
+        settings.append("; Your LiveRoom ")
+        print("Your LiveRoom")
+        settings_url = input("完整链接: ")
+        settings.append("LIVEROOM = " + settings_url + "\n")
+
         settings.append("; Name of the application you run the file from, I suggest VBA")
         print("Name of the application you run the file from, if Visual Boy Advance use VisualBoyAdvance")
         settings_app = input("Application name: ")
         settings.append("APP = " + settings_app + "\n")
-        
+
         settings.append("; The maximum number of lines in commands.txt (Useful for showing commands received in stream)")
         print("The maximum number of lines in commands.txt (Useful for showing commands received in stream)")
         settings_length = input("Length: ")
         settings.append("LENGTH = " + settings_length + "\n")
-        
-        settings.append("; Oh how to explain this...")
-        settings.append("; You get the chat command 'Left'")
-        settings.append("; You are currently facing right")
-        settings.append("; If QUICK_PRESS is true you turn left")
-        settings.append("; If QUICK_PRESS is false you turn left and move one square left")
-        print("Oh how to explain this...")
-        print("You get the chat command 'Left'")
-        print("You are currently facing right")
-        print("If QUICK_PRESS is true you turn left")
-        print("If QUICK_PRESS is false you turn left and move one square left")
+
+        settings.append("; 是否直接转向")
+        print("是否直接转向")
         settings_press = input("QUICK PRESS: ")
         settings.append("QUICK_PRESS = " + settings_press + "\n")
-        
+
         with open("settings.txt", "w") as f:
             for each_setting in settings:
                 f.write(each_setting + '\n')
@@ -66,111 +85,117 @@ while True:
         democracy_time = float(input("(must be integer) X="))
         break
 
-# 今儿就看到这吧
 # Anarchy Game Mode
 if mode.lower() == "anarchy":
     with open("lastsaid.txt", "w") as f:
         f.write("")
-        
+
     print("Starting %s" % GAME)
     time.sleep(1)
     emulator_job = Thread(target = startemulator, args = ())
     emulator_job.start()
-    
-    s=socket.socket( )
-    s.connect((HOST, PORT))
 
-    s.send(bytes("PASS %s\r\n" % AUTH, "UTF-8"))
-    s.send(bytes("NICK %s\r\n" % NICK, "UTF-8"))
-    s.send(bytes("USER %s %s bla :%s\r\n" % (NICK, HOST, NICK), "UTF-8"))
-    s.send(bytes("JOIN #%s\r\n" % CHAT_CHANNEL, "UTF-8"));
-    s.send(bytes("PRIVMSG #%s :Connected\r\n" % CHAT_CHANNEL, "UTF-8"))
-    print("Sent connected message to channel %s" % CHAT_CHANNEL)
+    dmc = DanMuClient(URL)
+    if not dmc.isValid(): print('Url not valid')
 
-    while 1:
-        readbuffer = readbuffer+s.recv(1024).decode("UTF-8", errors="ignore")
-        temp = str.split(readbuffer, "\n")
-        readbuffer=temp.pop( )
+    # @dmc.danmu
+    # def danmu_fn(msg):
+    #     pp('[%s] %s' % (msg['NickName'], msg['Content']))
+    @dmc.danmu
+    def danmu_fn(msg):
+        shell.AppActivate("VisualBoyAdvance")
+        time.sleep(.02)
+        press('z')
+        # test = msg
+        # print(test)
+    dmc.start(blockThread = True)
 
-        for line in temp:
-            x = 0
-            out = ""
-            line = str.rstrip(line)
-            line = str.split(line)
+    # while 1:
 
-            for index, i in enumerate(line):
-                if x == 0:
-                    user = line[index]
-                    user = user.split('!')[0]
-                    user = user[0:12] + ": "
-                if x == 3:
-                    out += line[index]
-                    out = out[1:]
-                if x >= 4:
-                    out += " " + line[index]
-                x = x + 1
-            
-            # Respond to ping, squelch useless feedback given by twitch, print output and read to list
-            if user == "PING: ":
-                s.send(bytes("PONG tmi.twitch.tv\r\n", "UTF-8"))
-            elif user == ":tmi.twitch.tv: ":
-                pass
-            elif user == ":tmi.twitch.: ":
-                pass
-            elif user == ":%s.tmi.twitch.tv: " % NICK:
-                pass
-            else:
-                try:
-                    print(user + out)
-                except UnicodeEncodeError:
-                    print(user)
-                
-            # Take in output
-            if out.lower() == 'up':
-                shell.AppActivate("%s" % APP)
-                time.sleep(.02)
-                press('up_arrow')
-                addtofile()
-            if out.lower() == 'right':
-                shell.AppActivate("%s" % APP)
-                time.sleep(.02)
-                press('right_arrow')
-                addtofile()
-            if out.lower() == 'down':
-                shell.AppActivate("%s" % APP)
-                time.sleep(.02)
-                press('down_arrow')
-                addtofile()
-            if out.lower() == 'left':
-                shell.AppActivate("%s" % APP)
-                time.sleep(.02)
-                press('left_arrow')
-                addtofile()
-            if out.lower() == 'a':
-                shell.AppActivate("%s" % APP)
-                time.sleep(.02)
-                press('z')
-                addtofile()
-            if out.lower() == 'b':
-                shell.AppActivate("%s" % APP)
-                time.sleep(.02)
-                press('x')
-                addtofile()
-            if out.lower() == 'start':
-                shell.AppActivate("%s" % APP)
-                time.sleep(.02)
-                press('enter')
-                addtofile()
-            if out.lower() == 'select':
-                shell.AppActivate("%s" % APP)
-                time.sleep(.02)
-                press('backspace')
-                addtofile()
-                
-            # Write to file for stream view
-            with open("commands.txt", "w") as f:
-                for item in commands:
-                    f.write(item + '\n')
+    #     print (test)
+    #     readbuffer = readbuffer+s.recv(1024).decode("UTF-8", errors="ignore")
+    #     temp = str.split(readbuffer, "\n")
+    #     readbuffer=temp.pop( )
+
+    #     for line in temp:
+    #         x = 0
+    #         out = ""
+    #         line = str.rstrip(line)
+    #         line = str.split(line)
+
+    #         for index, i in enumerate(line):
+    #             if x == 0:
+    #                 user = line[index]
+    #                 user = user.split('!')[0]
+    #                 user = user[0:12] + ": "
+    #             if x == 3:
+    #                 out += line[index]
+    #                 out = out[1:]
+    #             if x >= 4:
+    #                 out += " " + line[index]
+    #             x = x + 1
+
+    #         # Respond to ping, squelch useless feedback given by twitch, print output and read to list
+    #         if user == "PING: ":
+    #             s.send(bytes("PONG tmi.twitch.tv\r\n", "UTF-8"))
+    #         elif user == ":tmi.twitch.tv: ":
+    #             pass
+    #         elif user == ":tmi.twitch.: ":
+    #             pass
+    #         elif user == ":%s.tmi.twitch.tv: " % NICK:
+    #             pass
+    #         else:
+    #             try:
+    #                 print(user + out)
+    #             except UnicodeEncodeError:
+    #                 print(user)
+
+    #         # Take in output
+    #         if out.lower() == 'up':
+    #             shell.AppActivate("%s" % APP)
+    #             time.sleep(.02)
+    #             press('up_arrow')
+    #             addtofile()
+    #         if out.lower() == 'right':
+    #             shell.AppActivate("%s" % APP)
+    #             time.sleep(.02)
+    #             press('right_arrow')
+    #             addtofile()
+    #         if out.lower() == 'down':
+    #             shell.AppActivate("%s" % APP)
+    #             time.sleep(.02)
+    #             press('down_arrow')
+    #             addtofile()
+    #         if out.lower() == 'left':
+    #             shell.AppActivate("%s" % APP)
+    #             time.sleep(.02)
+    #             press('left_arrow')
+    #             addtofile()
+    #         if out.lower() == 'a':
+    #             shell.AppActivate("%s" % APP)
+    #             time.sleep(.02)
+    #             press('z')
+    #             addtofile()
+    #         if out.lower() == 'b':
+    #             shell.AppActivate("%s" % APP)
+    #             time.sleep(.02)
+    #             press('x')
+    #             addtofile()
+    #         if out.lower() == 'start':
+    #             shell.AppActivate("%s" % APP)
+    #             time.sleep(.02)
+    #             press('enter')
+    #             addtofile()
+    #         if out.lower() == 'select':
+    #             shell.AppActivate("%s" % APP)
+    #             time.sleep(.02)
+    #             press('backspace')
+    #             addtofile()
+
+    #         # Write to file for stream view
+    #         with open("commands.txt", "w") as f:
+    #             for item in commands:
+    #                 f.write(item + '\n')
 
 # Democracy Game Mode
 if mode.lower() == "democracy":
@@ -180,7 +205,7 @@ if mode.lower() == "democracy":
     count_job = Thread(target = democracy, args = ())
     count_job.start()
     #count_job.join()
-    
+
     print("Starting %s" % GAME)
     time.sleep(1)
     emulator_job = Thread(target = startemulator, args = ())
@@ -188,7 +213,7 @@ if mode.lower() == "democracy":
 
     s=socket.socket( )
     s.connect((HOST, PORT))
-    
+
     s.send(bytes("PASS %s\r\n" % AUTH, "UTF-8"))
     s.send(bytes("NICK %s\r\n" % NICK, "UTF-8"))
     s.send(bytes("USER %s %s bla :%s\r\n" % (NICK, HOST, NICK), "UTF-8"))
@@ -200,7 +225,7 @@ if mode.lower() == "democracy":
         readbuffer = readbuffer+s.recv(1024).decode("UTF-8", errors="ignore")
         temp = str.split(readbuffer, "\n")
         readbuffer=temp.pop( )
-                
+
         for line in temp:
             x = 0
             out = ""
@@ -218,7 +243,7 @@ if mode.lower() == "democracy":
                 if x >= 4:
                     out += " " + line[index]
                 x = x + 1
-            
+
             # Respond to ping, squelch useless feedback given by twitch, print output and read to list
             if user == "PING: ":
                 s.send(bytes("PONG tmi.twitch.tv\r\n", "UTF-8"))
@@ -233,7 +258,7 @@ if mode.lower() == "democracy":
                     print(user + out)
                 except UnicodeEncodeError:
                     print(user)
-                
+
             # Take in output
             if out.lower() == 'up':
                 addtofile()
@@ -251,7 +276,7 @@ if mode.lower() == "democracy":
                 addtofile()
             if out.lower() == 'select':
                 addtofile()
-                
+
             # Write to file for stream view
             with open("commands.txt", "w") as f:
                 for item in commands:
