fd43398acb2f4bd56bc30ca8109584f87a437ad1
Hero <www@eyenobig.com>
2017-04-21 18:27:16 +0800

    修改了很多 但是没有整理

--- Run.py
+++ Run.py
@@ -9,16 +9,15 @@ from danmu import DanMuClient
 
 settings = []
 shell = win32com.client.Dispatch("WScript.Shell")
-match = ['x','y','z']
-retime = time.time()
+match = ["anarchy","democracy",'x','y','z']
 # 选择游戏
 GAME = os.listdir('game')[0]
-
 def Stat(data):
     obj = "{"
     for v in data:
-        obj += '"' + v + '":"0",' 
+        obj += '"' + v + '":0,'
     obj = eval(obj[:-1] + '}')
+    return obj
 def remsg(msg):
     return msg.encode(sys.stdin.encoding, 'ignore').decode(sys.stdin.encoding)
 def startemulator():
@@ -36,6 +35,101 @@ def press(*args):
         if QUICK_PRESS == True:
             time.sleep(.01)
         win32api.keybd_event(VK_CODE[i],0 ,win32con.KEYEVENTF_KEYUP ,0)
+def Maxconut(data):
+    max = 0
+    key = ''
+    for k in data:
+        if data[k] >= max:
+            max = data[k]
+            key = k
+    return key
+def Model_Recod(data):
+    Sum = 0
+    for v in data:
+        Sum += data[v]
+    if data['anarchy']:
+        if data['anarchy']/Sum >= 0.6:
+            return 'anarchy'
+    if data['democracy']:
+        if data['democracy']/Sum >= 0.5:
+            return 'democracy'
+    else:
+        return 0
+
+def Time_load(Model):
+    while True:
+        global retime
+        global Log
+
+        if time.time() > retime + democracy_time:
+            retime = time.time()
+            # print("到点了")
+            print(Log)
+            # print(Time_re.__dict__)
+            NewModel = Model_Recod(Log)
+            if NewModel:
+                ChangeModel(NewModel,Model)
+            Fbutton = Maxconut(Log)
+            Log = Stat(match)
+            shell.AppActivate("VisualBoyAdvance")
+            time.sleep(.02)
+            press(Fbutton)
+            print("投票结果" + Fbutton)
+            record = time.strftime("%Y%m%d_%H", time.localtime())
+            with open("record/"+record+".json", "a") as f:
+                f.write(Fbutton + '\n')
+def Time_Change(Model):
+
+    while True:
+        global retime
+        global Log
+        if time.time() > retime + democracy_time:
+            retime = time.time()
+            NewModel = Model_Recod(Log)
+            Log = Stat(match)
+            if NewModel:
+                ChangeModel(NewModel,Model)
+def PlayGame(Model):
+    global Time_re
+    global retime
+    global Log
+    dmc = DanMuClient(URL)
+    if not dmc.isValid(): print('Url not valid')
+    Model = Model.lower()
+    retime = time.time()
+    Log = Stat(match)
+    if Time_Model == False and Model == 'anarchy' :
+        Time_re = Thread(target = Time_Change, args = (Model,))
+        Time_re.start()
+    if Time_Model == False and Model == 'democracy' :
+        Time_re = Thread(target = Time_load, args = (Model,))
+        Time_re.start()
+    @dmc.danmu
+    def danmu_fn(msg):
+        button = msg['Content'].lower()
+        print(button)
+        if button in match:
+            Log[button] += 1
+            if Model == 'anarchy':
+                shell.AppActivate("VisualBoyAdvance")
+                time.sleep(.02)
+                press(button)
+                record = time.strftime("%Y%m%d_%H", time.localtime())
+                with open("record/"+record+".json", "a") as f:
+                    f.write(button + '\n')
+            if Model == 'democracy':
+                Log[button] += 1
+    dmc.start(blockThread = True)
+
+def ChangeModel(Model,NowModel):
+    if Model == NowModel:
+        print("与现在模式相同")
+    if Model != NowModel:
+        print("切换模式中")
+        dmc.stop()
+        if Time_Model == False:
+            Time_re._is_stopped = True
+        PlayGame(Model)
 
 while True:
     if os.path.isfile("settings.txt"):
@@ -45,6 +139,7 @@ while True:
         URL = config.get('Settings', 'LIVEROOM').lower()
         APP = config.get('Settings', 'APP')
         QUICK_PRESS = config.getboolean('Settings', 'QUICK_PRESS')
+        Time_Model = config.getboolean('Settings', 'Time_Model')
         # CHAT_CHANNEL = config.get('Settings', 'CHAT_CHANNEL').lower()
         command_length = config.getint('Settings', 'LENGTH')
 
@@ -75,80 +170,35 @@ while True:
         settings_press = input("QUICK PRESS: ")
         settings.append("QUICK_PRESS = " + settings_press + "\n")
 
+        settings.append("; 1是弹幕记录时间 0是时间记录时间")
+        print("时间记录方式")
+        print("1是弹幕记录时间 0是时间记录时间")
+        settings_Time_Model = input("Time Model: ")
+        settings.append("Time_Model = " + settings_Time_Model + "\n")
+
         with open("settings.txt", "w") as f:
             for each_setting in settings:
                 f.write(each_setting + '\n')
 
 while True:
-    print("选择模式:  Democracy(民主),Anarchy(自由)")
-    mode = input("Game type: ")
-    if mode.lower() == "anarchy":
-        break
-    if mode.lower() == "democracy":
-        print("设置 X 秒 读取命令行: ")
-        democracy_time = float(input("(must be integer) X="))
-        break
-
-# Anarchy Game Mode
-if mode.lower() == "anarchy":
-
-    print("Starting %s" % GAME)
-    time.sleep(1)
-
-    emulator_job = Thread(target = startemulator, args = ())
-    emulator_job.start()
-
-    dmc = DanMuClient(URL)
-    if not dmc.isValid(): print('Url not valid')
-
-    @dmc.danmu
-    def danmu_fn(msg):
-        button = msg['Content'].lower()
-        print(button)    
-        if button in match:
-            shell.AppActivate("VisualBoyAdvance")
-            time.sleep(.02)
-            press(button)
-            record = time.strftime("%Y%m%d_%H", time.localtime())
-            with open("record/"+record+".json", "a") as f:
-                f.write(button + '\n')
-
-    dmc.start(blockThread = True)
-    
+    # print("选择模式:  Democracy(民主),Anarchy(自由)")
+    # mode = input("Game type: ")
+    # if mode.lower() == "anarchy":
+    #     break
+    # if mode.lower() == "democracy":
+    print("设置 X 秒 读取命令行: ")
+    democracy_time = float(input("(must be integer) X="))
+    break
 
 
-# Democracy Game Mode
-if mode.lower() == "democracy":
+print("Starting %s" % GAME)
+time.sleep(1)
+
+emulator_job = Thread(target = startemulator, args = ())
+emulator_job.start()
 
 
-    print("Starting %s" % GAME)
-    time.sleep(1)
-    emulator_job = Thread(target = startemulator, args = ())
-    emulator_job.start()
-    Log = Stat(match)
-    dmc = DanMuClient(URL)
-    if not dmc.isValid(): print('Url not valid')
-
-    retime = time.time()
 
 
-    @dmc.danmu
-    def danmu_fn(msg):
-        button = msg['Content'].lower()
-        print("弹幕时间"+ str(time.time()))
-        nowtime = time.time()
-        # print(retime)
-        if nowtime > retime + 20:
-            retime = nowtime
-        if button in match:
-            shell.AppActivate("VisualBoyAdvance")
-            time.sleep(.02)
-            Log['x'] += 1
-            
-            press(button)
-            record = time.strftime("%Y%m%d_%H", time.localtime())
-            with open("record/"+record+".json", "a") as f:
-                f.write(button + '\n')
-
-    dmc.start(blockThread = True)
-    
+# PlayGame(mode)
+PlayGame("Anarchy")
--- test.py
+++ test.py
@@ -1,38 +1,61 @@
-
-# match = {'x':'1','y':'1','z':'1'}
-match = ['x','y','z']
-
-def Stat(data):
-	obj = "{"
-	for v in data:
-		obj += '"' + v + '":0,' 
-	obj = eval(obj[:-1] + '}')	
-	return obj
-
-	
-def AddLog(v,Log):
-
-	Log[v] += 1
-
-	# Log[x] += 1
+# from mo import asd
 
 
-Log = Stat(match)
+match = {'x':9,'y':1,'z':1}
+# match = ['x','y','z']
+def Model_Recod(data):
+    Sum = 0
+    for v in data:
+        Sum += data[v]
+    print(data["x"]/Sum)
+    if data["x"]/Sum:
+        pass
+    # if data[v]/sum :
+    #     pass
+    return Sum
+    # data['free']
+Model_Recod(match)
+# def Stat(data):
+# 	obj = "{"
+# 	for v in data:
+# 		obj += '"' + v + '":0,'
+# 	obj = eval(obj[:-1] + '}')
+# 	return obj
 
-AddLog("x",Log)
-AddLog("x",Log)
-AddLog("x",Log)
-AddLog("x",Log)
-AddLog("x",Log)
-AddLog("x",Log)
-AddLog("x",Log)
 
-AddLog("y",Log)
-AddLog("y",Log)
-AddLog("y",Log)
-AddLog("y",Log)
-AddLog("y",Log)
-AddLog("y",Log)
 
-print (Log)
-print (max(Log))
+# def AddLog(v,Log):
+
+# 	Log[v] += 1
+
+# 	# Log[x] += 1
+
+
+# Log = Stat(match)
+
+# AddLog("x",Log)
+# AddLog("x",Log)
+# AddLog("z",Log)
+# AddLog("z",Log)
+# AddLog("z",Log)
+# AddLog("z",Log)
+# AddLog("z",Log)
+
+# AddLog("y",Log)
+# AddLog("y",Log)
+# AddLog("y",Log)
+# AddLog("z",Log)
+# AddLog("z",Log)
+# AddLog("z",Log)
+# AddLog("z",Log)
+
+# print (Log)
+# print (max(Log))
+
+
+
+
+
+
+
+
